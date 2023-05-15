0d6313c22286d0d4e8ab93b3af30e81547a0390b
정현우 <gusehd502@naver.com>
2023-05-15 17:45:04 +0900

    inclass

---
+++ SSAFY_Training/6. Vue.js/Vue.js_데일리과제/데일리 과제_vue_08_hw02.vue
@@ -0,0 +1,9 @@
+methods: {
+  login: function () {
+      axios(...생략...)
+      .then(res => {
+	    const token = res.data.access
+      localStorage.setItem('jwt',token)
+      })
+  }
+}
---
+++ SSAFY_Training/6. Vue.js/Vue.js_데일리과제/데일리 과제_vue_10_hw04.vue
@@ -0,0 +1,13 @@
+// App.vue
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      isLogin: false,
+    }
+  },
+  created: function () {
+    this.isLogin = localStorage.get('jwt')? true:false
+    }
+  },
